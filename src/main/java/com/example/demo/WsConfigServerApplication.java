4ed2f99fc8e257317ceddaa4bc26fce2cc5c7f66
Mouad Errafik Djenidi <mdj@audaxis.com>
2021-09-29 21:12:25 +0200

    first commit

---
+++ src/main/java/com/example/demo/WsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsConfigServerApplication.class, args);
+	}
+
+}
